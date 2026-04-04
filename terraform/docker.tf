8200432a9a03026837347bc9b790a59272e8f87d
Diebartdies <diego.r.carloni@gmail.com>
2026-04-04 02:30:36 -0300

    refactor(terraform): replace AWS with Docker provider and simplify config

---
+++ terraform/docker.tf
@@ -0,0 +1,15 @@
+resource "docker_image" "apache" {
+  name = "httpd:2.4"
+}
+
+resource "docker_container" "apache" {
+  name  = "apache"
+  image = docker_image.apache.image_id
+
+  ports {
+    internal = 80
+    external = 8585
+  }
+
+  restart_policy = "always"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,11 @@ terraform {
   required_version = ">= 1.5"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  # Uncomment to use remote state in S3
-  # backend "s3" {
-  #   bucket         = "your-terraform-state-bucket"
-  #   key            = "musicstore/terraform.tfstate"
-  #   region         = var.aws_region
-  #   dynamodb_table = "terraform-locks"
-  #   encrypt        = true
-  # }
-}
-
-provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = {
-      Project     = "musicstore"
-      Environment = var.environment
-      ManagedBy   = "terraform"
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
     }
   }
 }
 
-# ----------------------------------------------------------
-# Data sources
-# ----------------------------------------------------------
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-data "aws_caller_identity" "current" {}
+provider "docker" {}
